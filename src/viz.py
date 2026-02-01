3b782bd47c47d323f2747bbff17528e187a255de
Jeison Robles Arias <roblesjeison@gmail.com>
2026-02-01 17:01:25 -0600

    Adding Clustering Approaches and semantic label extraction

--- src/viz.py
+++ src/viz.py
@@ -30,3 +30,33 @@ def plot_embeddings_2d(
 
     fig.write_html(output_html)
     return fig
+
+
+def plot_embeddings_2d_Clusters(
+    df: pd.DataFrame,
+    output_html: str,
+    color_col : str= "Cluster"
+):
+    """
+    Create an interactive 2D scatter plot of embeddings with colors from learned clusters.
+
+    Required columns in df:
+    - x
+    - y
+    - paragraph_id
+    - snippet
+    """
+    fig = px.scatter(
+        df,
+        x="x",
+        y="y",
+        color=color_col,
+        hover_data={
+            "paragraph_id": True,
+            "snippet": True
+        },
+        title="Semantic Space of Moby-Dick Paragraphs (With CLusters)"
+    )
+
+    fig.write_html(output_html)
+    return fig
